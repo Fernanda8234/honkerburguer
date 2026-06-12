ad44ab7c831ff8d237017964846a2a1f7a3f54e1
ARYELY HEVYLYN DOS SANTOS SILVA <25203777@CFP127ALUNO.LOCAL>
2026-06-12 10:42:51 -0300

    CRUD da tabela ADM concluido

--- Back-End/app.js
+++ Back-End/app.js
@@ -19,18 +19,20 @@ const corsOptions = {
 app.use(cors(corsOptions))
   
 //Import das ROTAS do projeto
-const produtoRoutes     = require('./routes/produto.routes.js')
-const tipoSaborRoutes   = require('./routes/tipo_sabor.routes.js')
-const categoriaRoutes   = require('./routes/categoria.routes.js')
-const comboRoutes       = require('./routes/combos.routes.js')
+const produtoRoutes         = require('./routes/produto.routes.js')
+const tipoSaborRoutes       = require('./routes/tipo_sabor.routes.js')
+const categoriaRoutes       = require('./routes/categoria.routes.js')
+const comboRoutes           = require('./routes/combos.routes.js')
+const administradorRoutes   = require('./routes/administrador.routes.js')
 
 //Importação dos endpoints com o use
 app.use('/v1/senai/hamburgueria/produto', produtoRoutes)
 app.use('/v1/senai/hamburgueria/tipo_sabor', tipoSaborRoutes)
 app.use('/v1/senai/hamburgueria/categoria', categoriaRoutes)
 app.use('/v1/senai/hamburgueria/combo', comboRoutes)
+app.use('/v1/senai/hamburgueria/administrador', administradorRoutes)
 
-//Seve para inicializar a API para receber requisições
+//Serve para inicializar a API para receber requisições
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, function(){
---
+++ Back-End/model/DAO/administrador/administrador.js
@@ -0,0 +1,133 @@
+/********************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD no Banco de dados MySQL na tabela de administrador
+ * Data 12/06/2026
+ * Autor: Aryely Hevylyn
+ * Versão: 1.0
+ ********************************************************************************/
+
+//import da biblioteca para gerenciar o banco de dados Mysql no node.JS
+const knex = require('knex')
+
+//Import do arquivo de configuração para conexão com o BD Mysql
+const knexConfig = require('../../database_config_knex/knex_file.js')
+
+//Criar a conexão com o BD Mysql
+const knexConex = knex(knexConfig.development)
+
+const insertAdm = async function(administrador){
+    try {
+        let sql = `insert into tbl_administrador (
+                            nome,
+                            email,
+                            senha,
+                            data_nascimento,
+                            telefone,
+                            codigo_acesso 
+                            )
+                    values(
+                            '${administrador.nome}',
+                            '${administrador.email}',
+                            '${administrador.senha}',
+                            '${administrador.data_nascimento}',
+                            '${administrador.telefone}',
+                            '${administrador.codigo_acesso}'
+                            );`
+    
+        //Executar o scriptSQL no banco de dados
+        let result = await knexConex.raw(sql)
+    
+        if(result) return result[0].insertId //Retorna o ID gerado no banco de dados
+        else return false
+        
+        }catch(error){
+            console.log(error)
+            return false
+        }
+}
+
+const updateAdm = async function(administrador){
+        try {
+            // Script para atualizar os dados do BD
+            let sql = `update tbl_administrador set
+
+                    nome            = '${administrador.nome}',
+                    email           = '${administrador.email}',
+                    senha           = '${administrador.senha}',
+                    data_nascimento = '${administrador.data_nascimento}',
+                    telefone        = '${administrador.telefone}',
+                    codigo_acesso   = '${administrador.codigo_acesso}'
+                    where id        =  ${administrador.id}`
+                           
+                              
+            // Executa o script SQL no BD
+            let result = await knexConex.raw(sql)
+
+            if(result)
+                return true
+            else
+                return false
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+}
+
+const selectAllAdm = async function(){
+    try {
+        let sql = 'select * from tbl_administrador order by id desc'
+
+        let result = await knexConex.raw(sql)
+
+        //Validação para verificar se o retorno do banco é um array
+        //se o scriptSQL der erro, o banco não devolve um array
+        if(Array.isArray(result)){
+            return result[0]
+        }else return false
+
+    } catch (error){
+        console.log(error)
+        return false
+    }
+}
+
+const selectByIdAdm  = async function(id){
+    try {
+        let sql = `select * from tbl_administrador where id=${id}`
+
+        let result = await knexConex.raw(sql)
+
+        if(Array.isArray(result)){
+            return result[0]
+        }else return false
+
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+const deleteByIdAdm  = async function(id){
+    try{
+        let sql = `delete from tbl_administrador
+                     where id=${id}`
+
+    let result = await knexConex.raw(sql)
+
+    if(result){
+        return true
+    }else{
+        return false
+    }
+    }catch(error){
+        console.log(error)
+        return false
+    }
+}
+
+module.exports = {
+    insertAdm,
+    updateAdm,
+    selectAllAdm,
+    selectByIdAdm,
+    deleteByIdAdm
+}
