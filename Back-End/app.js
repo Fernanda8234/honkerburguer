b96b738a995347d84c090b6ae73bb5e6fcba0425
MATHEUS PEREIRA DE AGUIAR <25204557@cfp127aluno.local>
2026-06-17 10:34:05 -0300

    feat: finalizando a parte de integração: fazendo o create

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,21 +1,22 @@
-// Express e Dependências
+//Express para chamar o banco de dadods
+//Dependências
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
 const cors       = require('cors');
 
-// 🔥 CORREÇÃO PROTOCOLO 1: O CORS DEVE SER O PRIMEIRO MIDDLEWARE A SER ATIVADO
+//Configuração do cors
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With']
 }));
 
-// Ativação do interpretador de JSON
+//MiddleWares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Import das ROTAS do projeto
+//Importantando rotas
 const produtoRoutes         = require('./routes/produto.routes.js');
 const tipoSaborRoutes       = require('./routes/tipo_sabor.routes.js');
 const categoriaRoutes       = require('./routes/categoria.routes.js');
@@ -23,19 +24,20 @@ const comboRoutes           = require('./routes/combos.routes.js');
 const administradorRoutes   = require('./routes/administrador.routes.js');
 const produtoViewRoute      = require('./routes/vwproduto.routes.js'); // Importação da view
 
-// Configuração dos Endpoints
+//Configuração dos endpoints
 app.use('/v1/senai/hamburgueria/produto', produtoRoutes);
 app.use('/v1/senai/hamburgueria/tipo_sabor', tipoSaborRoutes);
 app.use('/v1/senai/hamburgueria/categoria', categoriaRoutes);
 app.use('/v1/senai/hamburgueria/combo', comboRoutes);
 app.use('/v1/senai/hamburgueria/administrador', administradorRoutes);
 
-// Endpoint da view (Garante que herda o CORS global acima)
+//View
 app.use('/v1/senai/hamburgueria/vw/produtos', produtoViewRoute);
 
-// Inicialização da API
+//Inicialização da API
 const PORT = process.env.PORT || 8080;
 
+//Porta dinâmica para que api consiga funcionar em qualquer localhost
 app.listen(PORT, function(){
     console.log('API Funcionando na porta ' + PORT);
 });
