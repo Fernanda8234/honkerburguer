433cbc9153336dfc22cb3b1f019022b480be981b
Fernanda8234 <fernanda82354769@gmail.com>
2026-06-18 11:03:02 -0300

    remove conflitos de merge

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,82 +1,41 @@
-//Express para chamar o banco de dadods
-//Dependências
-const express    = require('express');
-const app        = express();
-const bodyParser = require('body-parser');
-const cors       = require('cors');
-
-<<<<<<< HEAD
-//Criando um objeto para manipular dados do body da API em formato JSON
-const bodyParserJSON = bodyParser.json()
-
-//Cors
+//Express para chamar o banco de dados
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
-//CorsOptions para liberar o acesso a API para qualquer termo
-const corsOptions = {
-    origin: ['*'],
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: ['Content-type', 'Authorization']
-}
+//Configuração do cors
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With']
+}))
 
-app.use(cors(corsOptions))
-  
-//Import das ROTAS do projeto
-const produtoRoutes         = require('./routes/produto.routes.js')
-const tipoSaborRoutes       = require('./routes/tipo_sabor.routes.js')
-const categoriaRoutes       = require('./routes/categoria.routes.js')
-const comboRoutes           = require('./routes/combos.routes.js')
-const administradorRoutes   = require('./routes/administrador.routes.js')
+//MiddleWares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//Importação dos endpoints com o use
+//Importando rotas
+const produtoRoutes       = require('./routes/produto.routes.js')
+const tipoSaborRoutes     = require('./routes/tipo_sabor.routes.js')
+const categoriaRoutes     = require('./routes/categoria.routes.js')
+const comboRoutes         = require('./routes/combos.routes.js')
+const administradorRoutes = require('./routes/administrador.routes.js')
+const produtoViewRoute    = require('./routes/vwproduto.routes.js')
+
+//Endpoints
 app.use('/v1/senai/hamburgueria/produto', produtoRoutes)
 app.use('/v1/senai/hamburgueria/tipo_sabor', tipoSaborRoutes)
 app.use('/v1/senai/hamburgueria/categoria', categoriaRoutes)
 app.use('/v1/senai/hamburgueria/combo', comboRoutes)
 app.use('/v1/senai/hamburgueria/administrador', administradorRoutes)
 
-//Serve para inicializar a API para receber requisições
-const PORT = process.env.PORT || 8080
-=======
-//Configuração do cors
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With']
-}));
-
-//MiddleWares
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Importantando rotas
-const produtoRoutes         = require('./routes/produto.routes.js');
-const tipoSaborRoutes       = require('./routes/tipo_sabor.routes.js');
-const categoriaRoutes       = require('./routes/categoria.routes.js');
-const comboRoutes           = require('./routes/combos.routes.js');
-const administradorRoutes   = require('./routes/administrador.routes.js');
-const produtoViewRoute      = require('./routes/vwproduto.routes.js'); // Importação da view
-
-//Configuração dos endpoints
-app.use('/v1/senai/hamburgueria/produto', produtoRoutes);
-app.use('/v1/senai/hamburgueria/tipo_sabor', tipoSaborRoutes);
-app.use('/v1/senai/hamburgueria/categoria', categoriaRoutes);
-app.use('/v1/senai/hamburgueria/combo', comboRoutes);
-app.use('/v1/senai/hamburgueria/administrador', administradorRoutes);
-
 //View
-app.use('/v1/senai/hamburgueria/vw/produtos', produtoViewRoute);
+app.use('/v1/senai/hamburgueria/vw/produtos', produtoViewRoute)
 
-//Inicialização da API
-const PORT = process.env.PORT || 8080;
->>>>>>> back-end-tabelas
+//Porta dinâmica
+const PORT = process.env.PORT || 8080
 
-//Porta dinâmica para que api consiga funcionar em qualquer localhost
-app.listen(PORT, function(){
-<<<<<<< HEAD
+app.listen(PORT, function () {
     console.log('API Funcionando na porta ' + PORT)
 })
-=======
-    console.log('API Funcionando na porta ' + PORT);
-});
->>>>>>> back-end-tabelas
