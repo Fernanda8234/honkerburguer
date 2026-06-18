9a59464e05435236ed5df732bded133aa0ffe570
Fernanda8234 <fernanda82354769@gmail.com>
2026-06-18 19:57:09 -0300

    implementa layout da tela de exibição de produto

--- Front-End/app.js
+++ Front-End/app.js
@@ -1,12 +1,18 @@
 const URL = 'http://localhost:8080/v1/senai/hamburgueria/vw/produtos'
 
 export async function getProdutos() {
-    const response = await fetch (URL)
-    if (!response.ok) throw new Error('Erro ao buscar contatos')    
+    const response = await fetch(URL)
+
+    if (!response.ok)
+        throw new Error('Erro ao buscar produtos')
+
     return response.json()
-    
 }
 
-document.getElementById('btn-catalogo').addEventListener('click', () => {
-    window.location.href = "./tela-inicio/inicio.html"
-})
+const btnCatalogo = document.getElementById('btn-catalogo')
+
+if (btnCatalogo) {
+    btnCatalogo.addEventListener('click', () => {
+        window.location.href = "./tela-inicio/inicio.html"
+    })
+}
